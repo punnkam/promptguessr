96d5a26b4e10ded198489ce3292924f657b6880a
Punn Kam <punn@euler.xyz>
2023-02-25 16:51:48 -0800

    added next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,39 @@
-import './globals.css'
+import './globals.css';
+import { Session } from 'next-auth';
+import { headers } from 'next/headers';
+import AuthContext from './AuthContext';
 
-export default function RootLayout({
-  children,
+async function getSession(cookie: string): Promise<Session> {
+    const response = await fetch(
+        `${process.env.NEXTAUTH_URL}/api/auth/session`,
+        {
+            headers: {
+                cookie,
+            },
+        }
+    );
+
+    const session = await response.json();
+
+    return Object.keys(session).length > 0 ? session : null;
+}
+
+export default async function RootLayout({
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+    const session = await getSession(headers().get('cookie') ?? '');
+    return (
+        <html lang='en'>
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body>
+                <AuthContext session={session}>{children}</AuthContext>
+            </body>
+        </html>
+    );
 }
