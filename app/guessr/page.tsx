c327cf9164df03e11d1d30bb845393f6ad461efc
alexdphan <alexphan0515@gmail.com>
2023-02-26 01:44:15 -0500

    grammar

--- app/guessr/page.tsx
+++ app/guessr/page.tsx
@@ -126,9 +126,9 @@ export default function Home() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
                     <circle cx="12" cy="12" r="10"></circle>
                     <polyline points="12 16 16 12 12 8"></polyline>
