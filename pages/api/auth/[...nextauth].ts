da18287919edf6c1798977608431c39ecc6e7333
Punn Kam <punn@euler.xyz>
2023-03-08 10:09:47 -0800

    add: firebase credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import {
-  FirebaseAdapterConfig,
-  FirestoreAdapter,
+    FirebaseAdapterConfig,
+    FirestoreAdapter,
 } from '@next-auth/firebase-adapter';
 import firebaseConfig from '@/firebase.config';
 import 'firebase/firestore';
@@ -21,18 +21,24 @@ import { credential } from 'firebase-admin';
 
 // Initialize Firebase app
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 }
 
 export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
-  ],
-  adapter: FirestoreAdapter(), // using the GOOGLE_APPLICATION_CREDENTIALS environment variable
-  secret: process.env.NEXTAUTH_SECRET,
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
+    ],
+    adapter: FirestoreAdapter({
+        credential: credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        }),
+    }), // using the GOOGLE_APPLICATION_CREDENTIALS environment variable
+    secret: process.env.NEXTAUTH_SECRET,
 };
 export default NextAuth(authOptions);
