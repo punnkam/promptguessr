c3ad9a4450321638382546b8232d6c8e8ee88e78
Punn Kam <punn@euler.xyz>
2023-03-17 11:21:31 -0700

    fix: update stats on solve

--- components/ui/userstats.tsx
+++ components/ui/userstats.tsx
@@ -4,44 +4,49 @@ import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 
 const UserStats = () => {
-  const { data: session, status } = useSession();
-  const [rank, setRank] = useState<number>();
-  const [score, setScore] = useState<string>();
-  const [solved, setSolved] = useState<number>();
+    const { data: session, status } = useSession();
+    const [rank, setRank] = useState<number>();
+    const [score, setScore] = useState<string>();
+    const [solved, setSolved] = useState<number>();
+    const [stats, setStats] = useState<any>({});
 
-  useEffect(() => {
-    if (status !== 'authenticated') return;
-    const user: any = session?.user;
-    axios
-      .get(`/api/getUserStats?email=${user.email}`)
-      .then((res) => {
-        console.log(res.data);
-        setRank(res.data.rank);
-        setScore(res.data.score);
-        setSolved(res.data.solved);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [session, status]);
+    useEffect(() => {
+        if (status !== 'authenticated') return;
+        const user: any = session?.user;
+        axios
+            .get(`/api/getUserStats?email=${user.email}`)
+            .then((res) => {
+                console.log(res.data);
+                setStats({
+                    rank: res.data.rank,
+                    score: res.data.score,
+                    solved: res.data.solved,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [session, status, stats]);
 
-  return (
-    <div>
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>Rank: {rank}</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Score: {score}</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>
-            Solved: {solved}
-          </MenubarTrigger>
-        </MenubarMenu>
-      </Menubar>
-    </div>
-  );
+    return (
+        <div>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>Rank: {stats && stats.rank}</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>
+                        Score: {stats && stats.score}
+                    </MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>
+                        Solved: {stats && stats.solved}
+                    </MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+        </div>
+    );
 };
 
 export default UserStats;
